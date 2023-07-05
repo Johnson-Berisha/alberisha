c51bdca458a751026ebc6d3b4cd39ef6a0f5c808
Gjonson Berisha <gjonsonberisha@gmail.com>
2023-07-05 20:25:19 +0200

    a lot 2

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+// scroll
+var slideBottom = {
+    distance: '10%',
+    origin: 'left',
+    opacity: 0,
+    reset: true,
+    duration: 1500,
+    delay: 250
+  };
+
+  var slide2 = {
+    distance: '50%',
+    origin: 'top',
+    opacity: 0,
+    reset: true,
+    duration: 1500,
+    delay: 250
+  };
+
+  var slide3 = {
+    distance: '50%',
+    origin: 'right',
+    opacity: 0,
+    reset: true,
+    duration: 1500,
+    delay: 200
+  };
+  
+  ScrollReveal().reveal('.herodesc', slideBottom);
+
+//   ScrollReveal().reveal('.reasonstext', slide2);
+  ScrollReveal().reveal('.header', slide2, {delay: 0});
+
+  ScrollReveal().reveal('.card1', {distance: '10%',
+  origin: 'left',
+  opacity: 0,
+  reset: true,
+  duration: 1500,
+  delay: 50});
+  ScrollReveal().reveal('.card2', {distance: '10%',
+  origin: 'right',
+  opacity: 0,
+  reset: true,
+  duration: 1500,
+  delay: 100});
+  ScrollReveal().reveal('.card3', {distance: '10%',
+  origin: 'left',
+  opacity: 0,
+  reset: true,
+  duration: 1500,
+  delay: 150});
+  ScrollReveal().reveal('.card4', {distance: '10%',
+  origin: 'right',
+  opacity: 0,
+  reset: true,
+  duration: 1500,
+  delay: 200});
+  ScrollReveal().reveal('.aboutphoto', slideBottom);
+  ScrollReveal().reveal('.abouttext', slide3);
+  ScrollReveal().reveal('.servicecardinfo', slide3);
